f03c82185dece9dae5e2df7c12542fdefc2fb474
Beata Gonera <beataiwonagonera@gmail.com>
2023-11-03 14:28:55 +0100

    navigation and menu drawer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
-import type { Metadata } from 'next'
-import { Cormorant_Infant } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Cormorant_Infant } from "next/font/google";
+import "./globals.css";
+import { DrawerMobileMenu } from "./ui/organisms/DrawerMobileMenu";
+import { Navigation } from "./ui/organisms/Navigation";
 
-const cormorant = Cormorant_Infant({ weight: '700', subsets: ['latin'] })
+const cormorant = Cormorant_Infant({ weight: "700", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Cudny Kwiat',
-  description: 'Studio florystyczne - dekoracje ślubne, balonowe i świąteczne',
-}
+	title: "Cudny Kwiat",
+	description: "Studio florystyczne - dekoracje ślubne, balonowe i świąteczne",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="pl">
-      <body className={cormorant.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="pl">
+			<body className={cormorant.className}>
+				<Navigation />
+				{children}
+				<DrawerMobileMenu />
+			</body>
+		</html>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ const config = {
 	],
 	theme: {
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			colors: {
+				primary: "#EFE0DE",
+				fontprimary: "#141414",
 			},
 		},
 	},
